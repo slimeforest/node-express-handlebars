6211fd77ecb153b64bb9224e49053fa227df7fdb
jack battle <jackonno78@hotmail.com>
2021-02-10 21:17:29 -0600

    set up server file

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 9000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(routes);
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
